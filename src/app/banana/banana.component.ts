82151a5785b9d03586bcff6b1bee23622ab082c4
ssamoylov <ssamoylov@diasoft.ru>
2021-03-14 23:30:59 +0300

    init banana app

--- src/app/banana/banana.component.ts
+++ src/app/banana/banana.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {select, Store} from "@ngrx/store";
+import {AppState, getMyBanana} from "../app.state";
+import {GetNewBanana} from "./state";
 
 @Component({
     selector: 'app-banana',
@@ -9,16 +13,19 @@ export class BananaComponent implements OnInit {
 
     title = 'My NgRx Banana App';
 
-    constructor() {
+    banana$: Observable<any>;
+
+    constructor(private store: Store<AppState>) {
 
     }
 
     ngOnInit() {
-
+      this.newBanana();
+      this.banana$ = this.store.pipe(select(getMyBanana));
     }
 
     newBanana() {
-
+      this.store.dispatch(new GetNewBanana(null));
     }
 
     peelBanana() {
